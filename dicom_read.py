898f900838eb32de25e2b41e7a1f15d03ecd8532
dimitheodoro <42531867+dimitheodoro@users.noreply.github.com>
2022-09-24 15:14:22 +0300

    Update dicom_read.py

--- dicom_read.py
+++ dicom_read.py
@@ -4,6 +4,83 @@ from scipy import ndimage
 import  pydicom as dcm
 import matplotlib.pyplot as plt
 
+
+
+############################  WITH  SORTED SLICCES ######################################## 
+
+
+path ='lung-ct.volume-3d'
+
+def sort_dicoms(path):
+    acq_times = ([dcm.read_file(os.path.join(path,slice))[('0008','0032')].value for slice in (os.listdir((path)))])
+    scans_times = dict(zip(os.listdir(path),acq_times))
+    scans_times = {k: v for k, v in sorted(scans_times.items(), key=lambda item: item[1])}
+    sorted_dcm = list(scans_times.keys())
+    return sorted_dcm
+
+sorted_dcm = sort_dicoms(path)
+
+
+# Function to take care of teh translation and windowing. 
+def window_image(img, window_center,window_width, intercept, slope, rescale=False):
+    img = (img*slope +intercept) #for translation adjustments given in the dicom file. 
+    img_min = window_center - window_width//2 #minimum HU level
+    img_max = window_center + window_width//2 #maximum HU level
+    img[img<img_min] = img_min #set img_min for all HU levels less than minimum HU level
+    img[img>img_max] = img_max #set img_max for all HU levels higher than maximum HU level
+    if rescale: 
+        img = (img - img_min) / (img_max - img_min)*255.0 
+    return img
+    
+def get_first_of_dicom_field_as_int(x):
+    #get x[0] as in int is x is a 'pydicom.multival.MultiValue', otherwise get int(x)
+    if type(x) == dcm.multival.MultiValue: return int(x[0])
+    else: return int(x)
+    
+def get_windowing(data):
+    dicom_fields = [data[('0028','1050')].value, #window center
+                    data[('0028','1051')].value, #window width
+                    data[('0028','1052')].value, #intercept
+                    data[('0028','1053')].value] #slope
+    return [get_first_of_dicom_field_as_int(x) for x in dicom_fields]
+
+
+def read_dicom_images(path,sorted_dcm):
+    scans = [dcm.read_file(os.path.join(path,slice)) for slice in sorted_dcm]
+    slices =  np.array([dcm.read_file(os.path.join(path,slice)).pixel_array for slice in sorted_dcm])
+    # slices =np.transpose(slices,(1,2,0))
+    window_center , window_width, intercept, slope = get_windowing(scans[0])  
+    return  window_image(slices,window_center , window_width, intercept, slope )
+
+
+def process_scan(path,sorted_dcm):
+    """Read and resize volume"""
+    # Read scan
+    volume = read_dicom_images(path,sorted_dcm)
+    # Resize width, height and depth
+    return volume
+
+slices =process_scan('lung-ct.volume-3d',sorted_dcm)
+
+
+print(slices.shape)
+
+#############################################################################################
+
+#############################################################################################
+#############################################################################################
+
+#############################################################################################
+#############################################################################################
+
+#############################################################################################
+#############################################################################################
+
+#############################################################################################
+
+############################  WITHOUT SORTED SLICCES ########################################
+
+
 # Function to take care of teh translation and windowing. 
 def window_image(img, window_center,window_width, intercept, slope, rescale=True):
     img = (img*slope +intercept) #for translation adjustments given in the dicom file. 
