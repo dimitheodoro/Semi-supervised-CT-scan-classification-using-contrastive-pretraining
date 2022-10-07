f8630c49be10d022968a3eaad568ff099d5cafdd
dimitheodoro <42531867+dimitheodoro@users.noreply.github.com>
2022-10-07 07:44:19 +0300

    Update dicom_read.py

--- dicom_read.py
+++ dicom_read.py
@@ -1,3 +1,100 @@
+
+################################################### 3D
+# !pip install pydicom 
+import  pydicom as dcm
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+path ='test'
+
+
+def sort_dicoms(path):
+    acq_times = ([dcm.read_file(os.path.join(path,slice))[('0008','0032')].value for slice in (os.listdir((path)))])
+    scans_times = dict(zip(os.listdir(path),acq_times))
+    scans_times = {k: v for k, v in sorted(scans_times.items(), key=lambda item: item[1])}
+    sorted_dcm = list(scans_times.keys())
+    return sorted_dcm
+
+# sorted_dcm = sort_dicoms(path)
+
+def window_image(img, window_center,window_width, intercept, slope, rescale=True):
+    img = (img*slope +intercept) #for translation adjustments given in the dicom file. 
+    img_min = window_center - window_width//2 #minimum HU level
+    img_max = window_center + window_width//2 #maximum HU level
+    img[img<img_min] = img_min #set img_min for all HU levels less than minimum HU level
+    img[img>img_max] = img_max #set img_max for all HU levels higher than maximum HU level
+    if rescale: 
+        img = (img - img_min) / (img_max - img_min)*255.0 
+    return img
+    
+def get_first_of_dicom_field_as_int(x):
+    #get x[0] as in int is x is a 'pydicom.multival.MultiValue', otherwise get int(x)
+    if type(x) == dcm.multival.MultiValue: return int(x[0])
+    else: return int(x)
+    
+def get_windowing(data):
+    dicom_fields = [data[('0028','1050')].value, #window center
+                    data[('0028','1051')].value, #window width
+                    data[('0028','1052')].value, #intercept
+                    data[('0028','1053')].value] #slope
+    return [get_first_of_dicom_field_as_int(x) for x in dicom_fields]
+
+
+def read_dicom_images(path,sorted_dcm):
+    scans = [dcm.read_file(os.path.join(path,slice)) for slice in sorted_dcm]
+    slices =  np.array([dcm.read_file(os.path.join(path,slice)).pixel_array for slice in sorted_dcm])
+    # slices =np.transpose(slices,(1,2,0))
+    window_center , window_width, intercept, slope = get_windowing(scans[0])  
+    return  window_image(slices,window_center , window_width, intercept, slope )
+
+
+
+def process_scan(path,sorted_dcm):
+    """Read and resize volume"""
+    # Read scan
+    volume = read_dicom_images(path,sorted_dcm)
+    # Resize width, height and depth
+
+    return volume.astype('float32')
+
+slices=[]
+sorted_dcms=[]
+for patient in os.listdir(path):
+    sorted_dcms.append(sort_dicoms(os.path.join(path,patient)))
+
+sorted_dcms= np.array(sorted_dcms)
+sorted_dcms.shape
+
+for i,patient in enumerate (os.listdir(path)):
+
+    slices.append(process_scan(os.path.join(path,patient),sorted_dcms[i]))
+
+slices= np.array(slices)
+slices.shape
+
+
+
+
+
+############# xreiazetai resampling
+
+
+
+
+
+
+
+
+
+
+
+
+
+######################################################## 2D
+
+
 import numpy as np
 from scipy import ndimage
 !pip install pydicom
